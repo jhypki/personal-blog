b414abe64a30481576abd796d2906917e36952aa
Jakub Hypki <kubahypki7@gmail.com>
2024-04-06 17:45:34 +0200

    sanity

--- sanity/lib/client.js
+++ sanity/lib/client.js
@@ -1,10 +1,12 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
+import { apiVersion, dataset, projectId, useCdn } from "../env";
 
 export const client = createClient({
   apiVersion,
   dataset,
   projectId,
   useCdn,
-})
+  token:
+    "sk8fClbDuWMpn6qWJaRvdyV5hk5zyS5nRfqbGWzyKlZlhIEBdldHmnokKicmZDYHBjHHJbVyzX4LnklJNBdvZavxolVjLLO6tlznox6qQxLCW55CERCTWra76EJTXjO6JcFKpa4nYMY6Mm1dIGbcitr9cnPx7k1eXQOLpIDSnaYQxjIL2ncv",
+});
--- src/app/author/[slug]/page.jsx
+++ src/app/author/[slug]/page.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
+import { client } from "../../../../sanity/lib/client";
 
-const AuthorPage = () => {
-  return <div>authorpage</div>;
-};
-
-export default AuthorPage;
+export default function AuthorPage({ params }) {
+  const { slug } = params;
+  const [author, setAuthor] = useState(null);
+  useEffect(() => {
+    const fetch = async () => {
+      const query = `*[_type == "author" && slug.current == "${slug}"][0]`;
+      try {
+        const currentAuthor = await client.fetch(query);
+        setAuthor(currentAuthor);
+        console.log(currentAuthor);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetch();
+  }, [slug]);
+  return <div>{author.name}</div>;
+}
