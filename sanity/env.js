64a62fe3985cdcc6e50b4801976ad47d57d744ef
Jakub Hypki <kubahypki7@gmail.com>
2024-04-14 16:36:57 +0200

    env

--- sanity/env.js
+++ sanity/env.js
@@ -1,6 +1,6 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-03-30";
 
-export const dataset = "production";
-export const projectId = "n7q5pmx8";
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 export const useCdn = true;
